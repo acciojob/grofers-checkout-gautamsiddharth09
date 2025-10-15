ae4a3abb8466c1a139bbc7f290142a2ea2a1a386
Gautam kumar <gautamsiddharth2013@gmail.com>
2025-10-15 08:24:51 +0530

    created a submission

--- script.js
+++ script.js
@@ -4,6 +4,21 @@ document.body.appendChild(getSumBtn);
 
 const getSum = () => {
 //Add your code here
+	let selectPrice = document.querySelectorAll(".price");
+    let totalSum = 0;
+	selectPrice.forEach(p => {
+		totalSum += Number(p.innerText);
+		
+	});
+	let NewRow = document.createElement("tr");
+	let NewCell = document.createElement("td");
+	 
+	NewCell.innerText = `Total Price: ${totalSum}`;
+	NewRow.appendChild(NewCell);
+  document.getElementById("list").appendChild(NewRow);
+	
+	
+	
   
 };
 
